75fbd158c29d4fb00da9c44595d75f2207df1f3f
sreejith <sreejithsasidharan859@gmail.com>
2023-09-21 20:55:58 +0530

    vite cofigered

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,12 @@ import authRoutes from './routes/authRoute.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import cors from 'cors';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path'
+
 
 // Load environment variables from .env file
 dotenv.config();
 
-// Get the directory name using import.meta.url
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+
 
 // Database configuration and connection
 const mongoURI = process.env.MONGO_URL;
@@ -29,17 +25,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, './client/build')));
+
 
 //routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes);
-
-//rest api
-app.use('*', function(req, res) {
- res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
 
 //PORT
 const PORT = process.env.PORT || 8000;
