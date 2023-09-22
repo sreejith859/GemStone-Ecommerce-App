74539031ddd7107ea31e404e08857f7924605cf3
sreejith <sreejithsasidharan859@gmail.com>
2023-09-22 12:30:52 +0530

    vite config changed for chunks optimising

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1500, // Set the limit to 1500 kB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
 });
